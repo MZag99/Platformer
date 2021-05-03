69584b8e116f421d8b1091ce8438fa401c9e293d
Michał Zagojski <zagojski.michal.99@gmail.com>
2021-05-03 15:56:44 +0200

    Fix player and background quality and spritesheet animation

--- js/script.js
+++ js/script.js
@@ -10,8 +10,8 @@ const keys = [];
 const player = {
     x:0,
     y:0,
-    width:64,
-    height:64,
+    width:256,
+    height:256,
     frameX:0,
     frameY:0,
     speed: 9,
@@ -23,8 +23,9 @@ playerSprite.src = "images/spritesheet player.png";
 const background = new Image();
 background.src = "images/background.png";
 
-let offsetx = 0, offsety = 150;
-let positionx = 0, positiony = 300;
+
+let offsetx = 0, offsety = 0;
+let positionx = 0, positiony = 90;
 
 function drawPlayer(img, sX, sY, sW, sH, dX, dY, dW, dH){
     ctx.drawImage(img, sX, sY, sW, sH, dX, dY, dW, dH);
@@ -32,8 +33,8 @@ function drawPlayer(img, sX, sY, sW, sH, dX, dY, dW, dH){
 }
 
 function animate(){
-    ctx.drawImage(background, offsetx, offsety, 240, 150, 0, 0, canvas.width, canvas.height);
-    drawPlayer(playerSprite, 0, 0, player.height, player.width, positionx, positiony, player.height * 2, player.width * 2);
+    ctx.drawImage(background, offsetx, offsety, 800, 500, 0, 0, canvas.width, canvas.height);
+    drawPlayer(playerSprite, 0, 0, player.height, player.width, positionx, positiony, player.height * 0.75, player.width * 0.75);
     requestAnimationFrame(animate);
 }
 animate();
