9bb3e738d5995984e02ebcfb1a48e7b1b29748bb
Michał Zagojski <zagojski.michal.99@gmail.com>
2021-05-04 20:45:38 +0200

    Add moving animation and map border collision

--- js/script.js
+++ js/script.js
@@ -31,14 +31,6 @@ function drawPlayer(img, sX, sY, sW, sH, dX, dY, dW, dH){
 
 }
 
-function idleStance(){
-    if(player.frameX<1024){ 
-        player.frameX = player.frameX + player.width;
-    }
-    else if(player.frameX==1024){ 
-        player.frameX = 0;
-    }
-}
 window.setInterval(idleStance,200);
 
 function animate(){
@@ -46,35 +38,68 @@ function animate(){
     drawPlayer(playerSprite, player.frameX, player.frameY, player.height, player.width, player.x, player.y, player.height * 0.75, player.width * 0.75);
     moveBackground();
     movePlayer();
+    console.log(player.x);
     requestAnimationFrame(animate);
 }
 animate();
 
+function idleStance(){
+    if(player.frameX<1024 && player.moving == false){ 
+        player.frameX = player.frameX + player.width;
+    }
+    else if(player.frameX==1024 && player.moving == false){ 
+        player.frameX = 0;
+    }
+}
+
+function running(){
+    if(player.frameX<2304 && player.moving == true){ 
+        player.frameX = player.frameX + player.width;
+    }
+    else if(player.frameX==2304 && player.moving == true){ 
+        player.frameX = 0;
+    }
+}
+
+let fired = false;
+let runInterval = window.setInterval(running,100);
+
 window.addEventListener("keydown", function(e){
-    keys[e.keyCode] = true;
-    player.moving = true;
+    if(!fired){
+        fired = true;
+        keys[e.keyCode] = true;
+        player.moving = true;
+        runInterval;
+    }
 });
 window.addEventListener("keyup", function(e){
+    fired = false;
     delete keys[e.keyCode];
     player.moving = false;
+    player.frameY = 0;
+    player.frameX = 0;
 });
 
 function movePlayer(){
     if(keys[39]){
-        player.x += player.speed;
-        player.frameY = 0;
+        player.frameY = 512;
+        if(player.x < (canvas.width - player.width * 0.75 + 40)){
+            player.x += player.speed;
+        }
     }
     else if(keys[37]){
-        player.x -= player.speed;
-        player.frameY = 256;
+        player.frameY = 768;
+        if(player.x > -30){
+            player.x -= player.speed;
+        }
     }
 }
 function moveBackground(){
-    if(player.x >= 150 && player.moving == true){
+    if(player.x >= 150 && player.moving == true && offsetx < 800){
         player.x = 150;
         offsetx += player.speed;
     }
-    else if(player.x <= 0 && player.moving == true){
+    else if(player.x <= 0 && player.moving == true && offsetx > 0){
         player.x = 0;
         offsetx -= player.speed;
     }
